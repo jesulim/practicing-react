c482662c76cd801a5a4a6fb7fb685dbd774fa777
Ivan Martinez <jesulim@gmail.com>
2024-01-08 18:33:14 -0400

    Refactor TwitterFollowCard component

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -1,5 +1,48 @@
-import './App.css'
-import { TwitterFollowCard } from './TwitterFollowCard'
+// import './App.css'
+// import { TwitterFollowCard } from './TwitterFollowCard'
+
+// const users = [
+//   {
+//     userName: 'midudev',
+//     name: 'Miguel Ángel Durán',
+//     isFollowing: false
+//   },
+//   {
+//     userName: 'jesulim',
+//     name: 'Ivan Martinez',
+//     isFollowing: false
+//   },
+//   {
+//     userName: 'PacoHdezs',
+//     name: 'Paco Hdez',
+//     isFollowing: false
+//   },
+//   {
+//     userName: 'TMChein',
+//     name: 'Tomas',
+//     isFollowing: false
+//   }
+// ]
+
+// export function App () {
+//   return (
+//     <section className='bg-gray-900 text-white h-screen grid place-content-center gap-2'>
+//       {
+//         users.map(({ userName, name, isFollowing }) => (
+//           <TwitterFollowCard
+//             key={userName}
+//             userName={userName}
+//             initialIsFollowing={isFollowing}
+//           >
+//             {name}
+//           </TwitterFollowCard>
+//         )
+//         )
+//       }
+//     </section>
+//   )
+// }
+import TwitterFollowCard from './TwitterFollowCard.jsx'
 
 const users = [
   {
@@ -26,16 +69,15 @@ const users = [
 
 export function App () {
   return (
-    <section className='bg-gray-900 text-white h-screen grid place-content-center gap-2'>
+    <section className='bg-gray-900 text-white h-screen grid place-content-center gap-4'>
       {
         users.map(({ userName, name, isFollowing }) => (
           <TwitterFollowCard
             key={userName}
             userName={userName}
+            name={name}
             initialIsFollowing={isFollowing}
-          >
-            {name}
-          </TwitterFollowCard>
+          />
         )
         )
       }
--- projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
+++ projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
@@ -1,65 +1,102 @@
 import { useState } from 'react'
 
-export function TwitterFollowCard ({ children, userName, initialIsFollowing }) {
+export default function TwitterFollowCard ({ userName, name, initialIsFollowing }) {
   const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
-
-  const text = isFollowing ? 'Siguiendo' : 'Seguir'
 
   const handleClick = () => {
     setIsFollowing(!isFollowing)
   }
 
   return (
-    <>
-      {/* <article className='tw-followCard'>
-        <header className='tw-followCard-header'>
-          <img
-            className='tw-followCard-avatar'
-            alt='el avatar de midudev'
-            src={`https://unavatar.io/${userName}`}
-          />
-          <div className='tw-followCard-info'>
-            <strong>{children}</strong>
-            <span className='tw-followCard-infoUserName'>@{userName}</span>
-          </div>
-        </header>
-
-        <aside>
-          <button className={buttonClassName} onClick={handleClick}>
-            <span className='tw-followCard-text'>{text}</span>
-            <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
-          </button>
-        </aside>
-      </article> */}
-      <article className='flex justify-between text-sm gap-8'>
-        <header className='flex gap-2'>
-          <img
-            className='size-12 rounded-full'
-            src={`https://unavatar.io/${userName}`}
-            alt={`el avatar de ${userName}`}
-          />
-          <div className='grid'>
-            <strong>{children}</strong>
-            <span>{`@${userName}`}</span>
-          </div>
-        </header>
-        <aside className='grid items-center'>
-          <button
-            className={`cursor-pointer rounded-full px-4 py-1 border-2 transition duration-300 ease-in-out transform hover:opacity-80
-                ${isFollowing
-                ? 'border-solid border-2 border-gray-300 bg-transparent text-white w-140'
-                : ''
-              } ${isFollowing
-                ? 'hover:bg-red-100 hover:text-red-500 hover:border-red-500'
-                : ''
-              }`}
-            onClick={handleClick}
-          >
-            <span>{text}</span>
-            {/* <span>Dejar de seguir</span> */}
-          </button>
-        </aside>
-      </article>
-    </>
+    <article className='flex justify-between gap-16 text-sm'>
+      <header className='flex gap-2'>
+        <img
+          className='size-12 rounded-full'
+          src={`https://unavatar.io/${userName}`}
+          alt={`avatar de ${name}`}
+        />
+        <div className='grid'>
+          <span>{name}</span>
+          <span>{`@${userName}`}</span>
+        </div>
+      </header>
+      <aside className='grid items-center'>
+        <button
+          className={`rounded-full px-4 py-1 font-semibold
+          ${isFollowing
+              ? 'text-white border-2 hover:text-red-400 hover:border-red-400'
+              : 'text-black bg-gray-300 hover:bg-gray-400'}`}
+          onClick={handleClick}
+        >
+          <span>{isFollowing ? 'Siguiendo' : 'Seguir'}</span>
+        </button>
+      </aside>
+    </article>
   )
 }
+
+// import { useState } from 'react'
+
+// export function TwitterFollowCard ({ children, userName, initialIsFollowing }) {
+//   const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+
+//   const text = isFollowing ? 'Siguiendo' : 'Seguir'
+
+//   const handleClick = () => {
+//     setIsFollowing(!isFollowing)
+//   }
+
+//   return (
+//     <>
+//       {/* <article className='tw-followCard'>
+//         <header className='tw-followCard-header'>
+//           <img
+//             className='tw-followCard-avatar'
+//             alt='el avatar de midudev'
+//             src={`https://unavatar.io/${userName}`}
+//           />
+//           <div className='tw-followCard-info'>
+//             <strong>{children}</strong>
+//             <span className='tw-followCard-infoUserName'>@{userName}</span>
+//           </div>
+//         </header>
+
+//         <aside>
+//           <button className={buttonClassName} onClick={handleClick}>
+//             <span className='tw-followCard-text'>{text}</span>
+//             <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
+//           </button>
+//         </aside>
+//       </article> */}
+//       <article className='flex justify-between text-sm gap-8'>
+//         <header className='flex gap-2'>
+//           <img
+//             className='size-12 rounded-full'
+//             src={`https://unavatar.io/${userName}`}
+//             alt={`el avatar de ${userName}`}
+//           />
+//           <div className='grid'>
+//             <strong>{children}</strong>
+//             <span>{`@${userName}`}</span>
+//           </div>
+//         </header>
+//         <aside className='grid items-center'>
+//           <button
+//             className={`cursor-pointer rounded-full px-4 py-1 border-2 transition duration-300 ease-in-out transform hover:opacity-80
+//                 ${isFollowing
+//                 ? 'border-solid border-2 border-gray-300 bg-transparent text-white w-140'
+//                 : ''
+//               } ${isFollowing
+//                 ? 'hover:bg-red-100 hover:text-red-500 hover:border-red-500'
+//                 : ''
+//               }`}
+//             onClick={handleClick}
+//           >
+//             <span>{text}</span>
+//             {/* <span>Dejar de seguir</span> */}
+//           </button>
+//         </aside>
+//       </article>
+//     </>
+//   )
+// }
